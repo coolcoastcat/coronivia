fea07fb40a279be30e90eff92d6706519f74acf0
Russell Neville <russellneville@gmail.com>
2020-05-23 12:07:36 -0700

    WIP: Customizable Button component added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './logo.png';
 import './index.css';
+import { Button } from "./components/Button"
 
 class Square extends React.Component {
   render() {
@@ -12,43 +14,42 @@ class Square extends React.Component {
   }
 }
 
-class Board extends React.Component {
+class Game extends React.Component {
   renderSquare(i) {
     return <Square />;
   }
 
   render() {
-    const status = 'Next player: X';
 
+    const title = 'Welcome to Coronivia!'
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <div >{title}</div>
+          <Button onClick={() => {console.log("Clicked on Create Game")}}
+           type="button" 
+           buttonSize="btn--medium"
+           buttonStyle='btn--success--outline'>Create Game</Button>&nbsp;
+          <Button onClick={() => {console.log("Clicked on Join Game")}}
+           type="button" 
+           buttonSize="btn--medium"
+           buttonStyle='btn--success--solid'>Join Game</Button>
+       </div>
+      </header>
+    </div>
+      
     );
   }
 }
 
-class Game extends React.Component {
+class GameManager extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+      <div className="game-manager">
+        <div className="game">
+          <Game />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -62,7 +63,7 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <GameManager />,
   document.getElementById('root')
 );
 
